c355cd6807f4f169c076d90aa570f10245f623fc
nandeesh26 <106172032+nandeesh26@users.noreply.github.com>
2023-01-11 00:30:50 +0530

    Delete 1DBMS INVENTORY MANAGEMENT directory

--- 1DBMS INVENTORY MANAGEMENT/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-def create_db():
-    con=sqlite3.connect(database=r'DBMS.db')
-    cur=con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS employee(eid INTEGER PRIMARY KEY  AUTOINCREMENT,name text,email text,gender text,contact text,dob text,doj text,password text,utype text,address text,salary text)")
-    con.commit()
-    
-
-create_db()
